913c3cd72dc5bd99f3624a3ba32724da01e98d02
crvernon <chrisrvernon@gmail.com>
2018-05-09 11:22:53 -0400

    converted to class and eliminated the need for the ConfigSettings file and import

--- tethys/DataReader/IniReader.py
+++ tethys/DataReader/IniReader.py
@@ -10,155 +10,140 @@ Get the simulator settings from configuration(*.ini) file
 
 import os
 from configobj import ConfigObj
-from tethys.DataReader.ConfigSettings import ConfigSettings
 from tethys.Utils.exceptions import FileNotFoundError, DirectoryNotFoundError
 from tethys.Utils.Logging import Logger
 
 
-def getSimulatorSettings(iniFile):
-    
-    config = ConfigObj(iniFile)
-    settings = ConfigSettings()
+class Settings:
 
-    try:
-        settings.Logger = config['Logger']
-    except KeyError:
-        # No logger configuration.  Supply a default one for backward compatibility with old config files.
-        settings.Logger = {'logdir':'logs',
-                           'filename':'mainlog.txt'} 
+    def __init__(self, ini):
 
-    # project level params
-    settings.ProjectName        = config['Project']['ProjectName']
-    settings.InputFolder        = config['Project']['InputFolder']
-    settings.OutputFolder       = os.path.join(config['Project']['OutputFolder'], settings.ProjectName)
-    settings.rgnmapdir          = config['Project']['rgnmapdir']
-    settings.OutputFormat       = int(config['Project']['OutputFormat'])
-    settings.OutputUnit         = int(config['Project']['OutputUnit'])
-    settings.PerformDiagnostics = int(config['Project']['PerformDiagnostics'])
-    settings.PerformTemporal    = int(config['Project']['PerformTemporal'])
-    settings.subreg             = int(config['Project']['subreg'])
+        config = ConfigObj(ini)
 
-    # spatial params
-    try:
-        settings.SpatialResolution = float(config['Project']['SpatialResolution'])
-    except:
-        settings.SpatialResolution = 0.5
-    settings.mapsize = [int(180/settings.SpatialResolution), int(360/settings.SpatialResolution)]
+        try:
+            self.Logger = config['Logger']
+        except KeyError:
+            # No logger configuration.  Supply a default one for backward compatibility with old config files.
+            self.Logger = {'logdir': 'logs', 'filename': 'mainlog.txt'}
 
-    # GCAM access settings
-    settings.UseGCAMDatabase         = int(config['GCAM']['UseGCAMDatabase'])
-    if settings.UseGCAMDatabase:
-        settings.GCAM_DBpath         = os.path.join(settings.InputFolder, config['GCAM']['GCAM_DBpath'])
-        settings.GCAM_DBfile         = config['GCAM']['GCAM_DBfile']
-        settings.GCAM_query          = os.path.join(settings.GCAM_DBpath, config['GCAM']['GCAM_query'])
-        
-    else:
-        settings.read_irrS          = int(config['GCAM']['Read_irrS'])
-        settings.GCAM_CSV = config['GCAM']['GCAM_CSV']
+        # project level params
+        self.ProjectName        = config['Project']['ProjectName']
+        self.InputFolder        = config['Project']['InputFolder']
+        self.OutputFolder       = os.path.join(config['Project']['OutputFolder'], self.ProjectName)
+        self.rgnmapdir          = config['Project']['rgnmapdir']
+        self.OutputFormat       = int(config['Project']['OutputFormat'])
+        self.OutputUnit         = int(config['Project']['OutputUnit'])
+        self.PerformDiagnostics = int(config['Project']['PerformDiagnostics'])
+        self.PerformTemporal    = int(config['Project']['PerformTemporal'])
+        self.subreg             = int(config['Project']['subreg'])
 
-    settings.years              = config['GCAM']['GCAM_Years'] 
+        # spatial params
+        try:
+            self.SpatialResolution = float(config['Project']['SpatialResolution'])
+        except:
+            self.SpatialResolution = 0.5
+            self.mapsize = [int(180 / self.SpatialResolution), int(360 / self.SpatialResolution)]
 
-    # reference data
-    settings.Area               = os.path.join(settings.InputFolder, config['GriddedMap']['Area'])
-    settings.Coord              = os.path.join(settings.InputFolder, config['GriddedMap']['Coord'])
-    settings.aez                = os.path.join(settings.InputFolder, config['GriddedMap']['AEZ'])
-    settings.InputBasinFile     = os.path.join(settings.InputFolder, config['GriddedMap']['BasinIDs'])
-    settings.BasinNames         = os.path.join(settings.InputFolder, config['GriddedMap']['BasinNames'])
-    settings.gcam_basin_lu      = os.path.join(settings.InputFolder, config['GriddedMap']['GCAM_Basin_Key'])
-    settings.InputRegionFile    = os.path.join(settings.InputFolder, config['GriddedMap']['RegionIDs'])
-    settings.RegionNames        = os.path.join(settings.InputFolder, config['GriddedMap']['RegionNames'])
-    settings.InputCountryFile   = os.path.join(settings.InputFolder, config['GriddedMap']['CountryIDs'])
-    settings.CountryNames       = os.path.join(settings.InputFolder, config['GriddedMap']['CountryNames'])
-    settings.Population_GPW     = os.path.join(settings.InputFolder, config['GriddedMap']['Population_GPW'])
-    settings.Population_HYDE    = os.path.join(settings.InputFolder, config['GriddedMap']['Population_HYDE'])
-    settings.Irrigation_GMIA    = os.path.join(settings.InputFolder, config['GriddedMap']['Irrigation_GMIA'])
-    settings.Irrigation_HYDE    = os.path.join(settings.InputFolder, config['GriddedMap']['Irrigation_HYDE'])
-    settings.Livestock_Buffalo  = os.path.join(settings.InputFolder, config['GriddedMap']['Livestock_Buffalo'])
-    settings.Livestock_Cattle   = os.path.join(settings.InputFolder, config['GriddedMap']['Livestock_Cattle'])
-    settings.Livestock_Goat     = os.path.join(settings.InputFolder, config['GriddedMap']['Livestock_Goat'])
-    settings.Livestock_Sheep    = os.path.join(settings.InputFolder, config['GriddedMap']['Livestock_Sheep'])
-    settings.Livestock_Poultry  = os.path.join(settings.InputFolder, config['GriddedMap']['Livestock_Poultry'])
-    settings.Livestock_Pig      = os.path.join(settings.InputFolder, config['GriddedMap']['Livestock_Pig'])
-    settings.buff_fract         = os.path.join(settings.InputFolder, config['GriddedMap']['Buffalo_Fraction'])
-    settings.goat_fract         = os.path.join(settings.InputFolder, config['GriddedMap']['Goat_Fraction'])
-    settings.irrigated_fract    = os.path.join(settings.InputFolder, config['GriddedMap']['Irrigated_Fract'])
+        # GCAM access settings
+        self.UseGCAMDatabase = int(config['GCAM']['UseGCAMDatabase'])
+        if self.UseGCAMDatabase:
+            self.GCAM_DBpath = os.path.join(self.InputFolder, config['GCAM']['GCAM_DBpath'])
+            self.GCAM_DBfile = config['GCAM']['GCAM_DBfile']
+            self.GCAM_query = os.path.join(self.GCAM_DBpath, config['GCAM']['GCAM_query'])
 
-    if settings.PerformTemporal:
-        settings.TempMonthlyFile       = config['TemporalDownscaling']['Temp_Monthly']
-        settings.HDDCDDMonthlyFile     = config['TemporalDownscaling']['HDD_CDD_Monthly']
-        settings.Domestic_R            = config['TemporalDownscaling']['Domestic_R']
-        settings.Elec_Building         = config['TemporalDownscaling']['Elec_Building']
-        settings.Elec_Industry         = config['TemporalDownscaling']['Elec_Industry']
-        settings.Elec_Building_heat    = config['TemporalDownscaling']['Elec_Building_heat']
-        settings.Elec_Building_cool    = config['TemporalDownscaling']['Elec_Building_cool']
-        settings.Elec_Building_others  = config['TemporalDownscaling']['Elec_Building_others']
-        settings.Irr_MonthlyData       = config['TemporalDownscaling']['Irr_MonthlyData']
-        settings.TemporalInterpolation = int(config['TemporalDownscaling']['TemporalInterpolation'])
-    
-    CheckExistence(settings)
-    
-    return settings
+        else:
+            self.read_irrS = int(config['GCAM']['Read_irrS'])
+            self.GCAM_CSV = config['GCAM']['GCAM_CSV']
 
+        self.years = config['GCAM']['GCAM_Years']
 
-def PrintInfo(settings): 
+        # reference data
+        self.Area               = os.path.join(self.InputFolder, config['GriddedMap']['Area'])
+        self.Coord              = os.path.join(self.InputFolder, config['GriddedMap']['Coord'])
+        self.aez                = os.path.join(self.InputFolder, config['GriddedMap']['AEZ'])
+        self.InputBasinFile     = os.path.join(self.InputFolder, config['GriddedMap']['BasinIDs'])
+        self.BasinNames         = os.path.join(self.InputFolder, config['GriddedMap']['BasinNames'])
+        self.gcam_basin_lu      = os.path.join(self.InputFolder, config['GriddedMap']['GCAM_Basin_Key'])
+        self.InputRegionFile    = os.path.join(self.InputFolder, config['GriddedMap']['RegionIDs'])
+        self.RegionNames        = os.path.join(self.InputFolder, config['GriddedMap']['RegionNames'])
+        self.InputCountryFile   = os.path.join(self.InputFolder, config['GriddedMap']['CountryIDs'])
+        self.CountryNames       = os.path.join(self.InputFolder, config['GriddedMap']['CountryNames'])
+        self.Population_GPW     = os.path.join(self.InputFolder, config['GriddedMap']['Population_GPW'])
+        self.Population_HYDE    = os.path.join(self.InputFolder, config['GriddedMap']['Population_HYDE'])
+        self.Irrigation_GMIA    = os.path.join(self.InputFolder, config['GriddedMap']['Irrigation_GMIA'])
+        self.Irrigation_HYDE    = os.path.join(self.InputFolder, config['GriddedMap']['Irrigation_HYDE'])
+        self.Livestock_Buffalo  = os.path.join(self.InputFolder, config['GriddedMap']['Livestock_Buffalo'])
+        self.Livestock_Cattle   = os.path.join(self.InputFolder, config['GriddedMap']['Livestock_Cattle'])
+        self.Livestock_Goat     = os.path.join(self.InputFolder, config['GriddedMap']['Livestock_Goat'])
+        self.Livestock_Sheep    = os.path.join(self.InputFolder, config['GriddedMap']['Livestock_Sheep'])
+        self.Livestock_Poultry  = os.path.join(self.InputFolder, config['GriddedMap']['Livestock_Poultry'])
+        self.Livestock_Pig      = os.path.join(self.InputFolder, config['GriddedMap']['Livestock_Pig'])
+        self.buff_fract         = os.path.join(self.InputFolder, config['GriddedMap']['Buffalo_Fraction'])
+        self.goat_fract         = os.path.join(self.InputFolder, config['GriddedMap']['Goat_Fraction'])
+        self.irrigated_fract    = os.path.join(self.InputFolder, config['GriddedMap']['Irrigated_Fract'])
 
-    log = Logger.getlogger()
-    oldlvl = log.setlevel(Logger.INFO)
-    
-    log.write('Project Name        : {}\n'.format(settings.ProjectName))
-    log.write('Input Folder        : {}\n'.format(settings.InputFolder))
-    log.write('Output Folder       : {}\n'.format(settings.OutputFolder))
-    if settings.UseGCAMDatabase:
-        log.write('GCAM Database Folder: {}\n'.format(settings.GCAM_DBpath + settings.GCAM_DBfile))
-    else:
-        log.write('GCAM CSV Folder     : {}\n'.format(settings.GCAM_CSV))
-    log.write('Region Info Folder  : {}\n'.format(settings.rgnmapdir))
+        if self.PerformTemporal:
+            self.TempMonthlyFile       = config['TemporalDownscaling']['Temp_Monthly']
+            self.HDDCDDMonthlyFile     = config['TemporalDownscaling']['HDD_CDD_Monthly']
+            self.Domestic_R            = config['TemporalDownscaling']['Domestic_R']
+            self.Elec_Building         = config['TemporalDownscaling']['Elec_Building']
+            self.Elec_Industry         = config['TemporalDownscaling']['Elec_Industry']
+            self.Elec_Building_heat    = config['TemporalDownscaling']['Elec_Building_heat']
+            self.Elec_Building_cool    = config['TemporalDownscaling']['Elec_Building_cool']
+            self.Elec_Building_others  = config['TemporalDownscaling']['Elec_Building_others']
+            self.Irr_MonthlyData       = config['TemporalDownscaling']['Irr_MonthlyData']
+            self.TemporalInterpolation = int(config['TemporalDownscaling']['TemporalInterpolation'])
 
+        self.check_existence()
 
-def CheckExistence(settings): 
-    """
-    Check existence of input files and directories, and create output directory if necessary.
-    """
-    if not (os.path.exists(settings.InputFolder) and os.path.isdir(settings.InputFolder)):
-        raise DirectoryNotFoundError(settings.InputFolder)
+    def check_existence(self):
+        """
+        Check existence of input files and directories, and create output directory if necessary.
+        """
+        if not (os.path.exists(self.InputFolder) and os.path.isdir(self.InputFolder)):
+            raise DirectoryNotFoundError(self.InputFolder)
 
-    if not os.path.exists(settings.OutputFolder):
-        os.makedirs(settings.OutputFolder) # this will raise an exception if it fails to create the directory
-    
-    if not (os.path.exists(settings.rgnmapdir) and os.path.isdir(settings.rgnmapdir)):
-        raise DirectoryNotFoundError(settings.rgnmapdir)
+        if not os.path.exists(self.OutputFolder):
+            os.makedirs(self.OutputFolder)  # this will raise an exception if it fails to create the directory
 
-    # if settings.UseGCAMDatabase and not (os.path.exists(settings.GCAM_CSV) and os.path.isdir(settings.GCAM_CSV)):
-    #     raise DirectoryNotFoundError(settings.GCAM_CSV)
-        
-    # Check the existence of input files
-    strlist = ['Area', 'Coord', 'aez', 'InputBasinFile', 'BasinNames', 'InputRegionFile', 'RegionNames', 'InputCountryFile', \
-               'CountryNames', 'Population_GPW', 'Population_HYDE', 'Irrigation_GMIA', 'Irrigation_HYDE', \
-               'Livestock_Buffalo', 'Livestock_Cattle', 'Livestock_Goat', 'Livestock_Sheep', 'Livestock_Poultry', 'Livestock_Pig']
-    
-    ifn = 0
-    for s in strlist:
-        fn = getattr(settings, s)
-        if not os.path.isfile(fn):
-            raise FileNotFoundError(getattr(settings, s))
-               
-    if settings.PerformTemporal:
-        strlist = ['TempMonthlyFile', 'HDDCDDMonthlyFile', 'Domestic_R', 'Irr_MonthlyData', 'Elec_Building', 'Elec_Industry', \
-                   'Elec_Building_heat', 'Elec_Building_cool', 'Elec_Building_others']
+        if not (os.path.exists(self.rgnmapdir) and os.path.isdir(self.rgnmapdir)):
+            raise DirectoryNotFoundError(self.rgnmapdir)
+
+        # if settings.UseGCAMDatabase and not (os.path.exists(settings.GCAM_CSV) and os.path.isdir(settings.GCAM_CSV)):
+        #     raise DirectoryNotFoundError(settings.GCAM_CSV)
+
+        # Check the existence of input files
+        strlist = ['Area', 'Coord', 'aez', 'InputBasinFile', 'BasinNames', 'InputRegionFile', 'RegionNames',
+                   'InputCountryFile', \
+                   'CountryNames', 'Population_GPW', 'Population_HYDE', 'Irrigation_GMIA', 'Irrigation_HYDE', \
+                   'Livestock_Buffalo', 'Livestock_Cattle', 'Livestock_Goat', 'Livestock_Sheep', 'Livestock_Poultry',
+                   'Livestock_Pig']
+
+        ifn = 0
         for s in strlist:
-            fn = getattr(settings, s)
+            fn = getattr(self, s)
             if not os.path.isfile(fn):
-                raise FileNotFoundError(fn)
+                raise FileNotFoundError(getattr(self, s))
 
+        if self.PerformTemporal:
+            strlist = ['TempMonthlyFile', 'HDDCDDMonthlyFile', 'Domestic_R', 'Irr_MonthlyData', 'Elec_Building',
+                       'Elec_Industry', \
+                       'Elec_Building_heat', 'Elec_Building_cool', 'Elec_Building_others']
+            for s in strlist:
+                fn = getattr(self, s)
+                if not os.path.isfile(fn):
+                    raise FileNotFoundError(fn)
 
-def help():
-    
-    print 
-    '''
-    License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
-    Copyright (c) 2017, Battelle Memorial Institute
+    def print_info(self):
 
-    Module: DataReader.IniReader
-    
-    Read in configuration file (*.ini)
-    '''   
+        log = Logger.getlogger()
+        oldlvl = log.setlevel(Logger.INFO)
+
+        log.write('Project Name        : {}\n'.format(self.ProjectName))
+        log.write('Input Folder        : {}\n'.format(self.InputFolder))
+        log.write('Output Folder       : {}\n'.format(self.OutputFolder))
+        if self.UseGCAMDatabase:
+            log.write('GCAM Database Folder: {}\n'.format(self.GCAM_DBpath + self.GCAM_DBfile))
+        else:
+            log.write('GCAM CSV Folder     : {}\n'.format(self.GCAM_CSV))
+        log.write('Region Info Folder  : {}\n'.format(self.rgnmapdir))
