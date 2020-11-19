c71b44e195541f844aab184d86c98a49b1803bf5
Khan <zarrar.khan@pnnl.gov>
2020-11-18 19:26:41 -0500

    Updating docs

--- docs/conf.py
+++ docs/conf.py
@@ -25,10 +25,7 @@ sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'tethys','Utils'))
 
 # -- Autodoc initial settings -------------------------------------------------
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary']
 autosummary_generate = True
-templates_path = [ '_templates' ]
-exclude_patterns = ['_build', '_templates']
 autodoc_mock_imports = ["gcam_reader"]
 
 # -- Project information -----------------------------------------------------
@@ -51,7 +48,7 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_rtd_theme', 'sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+extensions = ['sphinx_rtd_theme', 'sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon','sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -59,7 +56,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','_templates']
 
 
 # -- Options for HTML output -------------------------------------------------
