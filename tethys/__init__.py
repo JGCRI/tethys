2a9eb003bec209b3fd9c44df749fcf63e939cf74
Zarrar Khan <zarrar.khan@pnnl.gov>
2021-05-21 16:34:23 -0400

    Updating install_supplement

--- tethys/__init__.py
+++ tethys/__init__.py
@@ -1,14 +1,4 @@
-"""
-@Date: 09/09/2017
-@author: Xinya Li (xinya.li@pnl.gov)
-@Project: Demeter-W V1.0
+from tethys.model import *
+from tethys.install_supplement import get_package_data
 
-License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
-Copyright (c) 2017, Battelle Memorial Institute
-
-Demeter-W: Spatial and Temporal Downscaling of Global Water Demands
-
-"""
-
-__author__ = "Xinya Li (xinya.li@pnnl.gov); Chris R. Vernon (chris.vernon@pnnl.gov)"
-__version__ = '1.0.0'
+__all__ = ['get_package_data','run_model']
--- tethys/install_supplement.py
+++ tethys/install_supplement.py
@@ -16,7 +16,7 @@ class InstallSupplement:
     """
 
     # URL for DOI minted example data hosted on Zenodo
-    DATA_VERSION_URLS = {'2.4.0': 'https://zenodo.org/record/2578287/files/example.zip?download=1'}
+    DATA_VERSION_URLS = {'1.3.0': 'https://zenodo.org/record/4776855/files/example_v1_3_0.zip?download=1'}
 
     def __init__(self, example_data_directory):
 
