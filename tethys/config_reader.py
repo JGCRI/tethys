3244b787ef58aa2e07413ec59495c0336c8fa45c
Isaac Thompson <93336839+ifthompson@users.noreply.github.com>
2022-04-22 10:05:26 -0600

    move grid mapping files into package, selected automatically based on GCAMUSA option

--- tethys/config_reader.py
+++ tethys/config_reader.py
@@ -74,24 +74,24 @@ class ReadConfig(Logger):
         self.GCAM_query = os.path.join(self.GCAM_DBpath, self.gcam_config.get('GCAM_query', 'None'))
         if self.PerformWithdrawal == 1 and not self.GCAM_query.endswith('.xml'):
             if self.GCAMUSA == 1:
-                self.GCAM_query = pkg_resources.resource_filename('tethys', 'reference/query_withdrawal_GCAMUSA.xml')
+                self.GCAM_query = pkg_resources.resource_filename('tethys', 'reference/queries/query_withdrawal_GCAMUSA.xml')
             else:
-                self.GCAM_query = pkg_resources.resource_filename('tethys', 'reference/query_withdrawal.xml')
+                self.GCAM_query = pkg_resources.resource_filename('tethys', 'reference/queries/query_withdrawal.xml')
 
         self.GCAM_query_C = os.path.join(self.GCAM_DBpath, self.gcam_config.get('GCAM_query_C', 'None'))
         if self.PerformConsumption == 1 and not self.GCAM_query_C.endswith('.xml'):
             if self.GCAMUSA == 1:
-                self.GCAM_query_C = pkg_resources.resource_filename('tethys', 'reference/query_consumption_GCAMUSA.xml')
+                self.GCAM_query_C = pkg_resources.resource_filename('tethys', 'reference/queries/query_consumption_GCAMUSA.xml')
             else:
-                self.GCAM_query_C = pkg_resources.resource_filename('tethys', 'reference/query_consumption.xml')
+                self.GCAM_query_C = pkg_resources.resource_filename('tethys', 'reference/queries/query_consumption.xml')
 
         self.GCAM_queryCore = os.path.join(self.GCAM_DBpath, self.gcam_config.get('GCAM_queryCore', 'None'))
-        if self.GCAMUSA == 1 and not self.GCAM_queryCore.endswith('.xml'):
-            self.GCAM_queryCore = pkg_resources.resource_filename('tethys', 'reference/query_withdrawal.xml')
+        if not self.GCAM_queryCore.endswith('.xml'):
+            self.GCAM_queryCore = pkg_resources.resource_filename('tethys', 'reference/queries/query_withdrawal.xml')
 
         self.GCAM_queryCore_C = os.path.join(self.GCAM_DBpath, self.gcam_config.get('GCAM_queryCore_C', 'None'))
-        if self.GCAMUSA == 1 and not self.GCAM_queryCore_C.endswith('.xml'):
-            self.GCAM_queryCore_C = pkg_resources.resource_filename('tethys', 'reference/query_consumption.xml')
+        if not self.GCAM_queryCore_C.endswith('.xml'):
+            self.GCAM_queryCore_C = pkg_resources.resource_filename('tethys', 'reference/queries/query_consumption.xml')
 
         self.subreg = int(self.gcam_config.get('GCAM_subreg', None))
 
@@ -104,15 +104,48 @@ class ReadConfig(Logger):
         # update any default dictionary values with items specified as arguments by the user
         self.map_params.update(self.params)
 
-        self.Area = os.path.join(self.InputFolder, self.map_params.get('Area'))
-        self.Coord = os.path.join(self.InputFolder, self.map_params.get('Coord'))
-        self.InputBasinFile = os.path.join(self.InputFolder, self.map_params.get('BasinIDs'))
-        self.BasinNames = os.path.join(self.InputFolder, self.map_params.get('BasinNames'))
-        self.gcam_basin_lu = os.path.join(self.InputFolder, self.map_params.get('GCAM_Basin_Key'))
-        self.InputRegionFile = os.path.join(self.InputFolder, self.map_params.get('RegionIDs'))
-        self.RegionNames = os.path.join(self.InputFolder, self.map_params.get('RegionNames'))
-        self.InputCountryFile = os.path.join(self.InputFolder, self.map_params.get('CountryIDs'))
-        self.CountryNames = os.path.join(self.InputFolder, self.map_params.get('CountryNames'))
+        self.Area = os.path.join(self.InputFolder, self.map_params.get('Area', 'None'))
+        if os.path.basename(self.Area) == 'None':
+            self.Area = pkg_resources.resource_filename('tethys', 'reference/grid_info/Grid_Areas_ID.csv')
+
+        self.Coord = os.path.join(self.InputFolder, self.map_params.get('Coord', 'None'))
+        if os.path.basename(self.Coord) == 'None':
+            self.Coord = pkg_resources.resource_filename('tethys', 'reference/grid_info/coordinates.csv')
+
+        self.InputBasinFile = os.path.join(self.InputFolder, self.map_params.get('BasinIDs', 'None'))
+        if os.path.basename(self.InputBasinFile) == 'None':
+            self.InputBasinFile = pkg_resources.resource_filename('tethys', 'reference/grid_info/basin.csv')
+
+        self.BasinNames = os.path.join(self.InputFolder, self.map_params.get('BasinNames', 'None'))
+        if os.path.basename(self.BasinNames) == 'None':
+            self.BasinNames = pkg_resources.resource_filename('tethys', 'reference/grid_info/BasinNames.csv')
+
+        self.gcam_basin_lu = os.path.join(self.InputFolder, self.map_params.get('GCAM_Basin_Key', 'None'))
+        if os.path.basename(self.gcam_basin_lu) == 'None':
+            self.gcam_basin_lu = pkg_resources.resource_filename('tethys', 'reference/grid_info/gcam_basin_lookup.csv')
+
+        self.InputRegionFile = os.path.join(self.InputFolder, self.map_params.get('RegionIDs', 'None'))
+        if os.path.basename(self.InputRegionFile) == 'None':
+            if self.GCAMUSA == 1:
+                self.InputRegionFile = pkg_resources.resource_filename('tethys', 'reference/grid_info/region32_grids_gcamUSA.csv')
+            else:
+                self.InputRegionFile = pkg_resources.resource_filename('tethys', 'reference/grid_info/region32_grids.csv')
+
+        self.RegionNames = os.path.join(self.InputFolder, self.map_params.get('RegionNames', 'None'))
+        if os.path.basename(self.RegionNames) == 'None':
+            if self.GCAMUSA == 1:
+                self.RegionNames = pkg_resources.resource_filename('tethys', 'reference/grid_info/RgnNames_gcamUSA.csv')
+            else:
+                self.RegionNames = pkg_resources.resource_filename('tethys', 'reference/grid_info/RgnNames.csv')
+
+        self.InputCountryFile = os.path.join(self.InputFolder, self.map_params.get('CountryIDs', 'None'))
+        if os.path.basename(self.InputCountryFile) == 'None':
+            self.InputCountryFile = pkg_resources.resource_filename('tethys', 'reference/grid_info/country.csv')
+
+        self.CountryNames = os.path.join(self.InputFolder, self.map_params.get('CountryNames', 'None'))
+        if os.path.basename(self.CountryNames) == 'None':
+            self.CountryNames = pkg_resources.resource_filename('tethys', 'reference/grid_info/country-names.csv')
+
         self.Population_GPW = os.path.join(self.InputFolder, self.map_params.get('Population_GPW'))
         self.Population_HYDE = os.path.join(self.InputFolder, self.map_params.get('Population_HYDE'))
 
@@ -125,15 +158,32 @@ class ReadConfig(Logger):
         self.Livestock_Sheep = os.path.join(self.InputFolder, self.map_params.get('Livestock_Sheep'))
         self.Livestock_Poultry = os.path.join(self.InputFolder, self.map_params.get('Livestock_Poultry'))
         self.Livestock_Pig = os.path.join(self.InputFolder, self.map_params.get('Livestock_Pig'))
-        self.buff_fract = os.path.join(self.InputFolder, self.map_params.get('Buffalo_Fraction'))
-        self.goat_fract = os.path.join(self.InputFolder, self.map_params.get('Goat_Fraction'))
-        self.irrigated_fract = os.path.join(self.InputFolder, self.map_params.get('Irrigated_Fract'))
+
+        self.buff_fract = os.path.join(self.InputFolder, self.map_params.get('Buffalo_Fraction', 'None'))
+        if os.path.basename(self.buff_fract) == 'None':
+            if self.GCAMUSA == 1:
+                self.buff_fract = pkg_resources.resource_filename('tethys', 'reference/grid_info/bfracFAO2005_gcamUSA.csv')
+            else:
+                self.buff_fract = pkg_resources.resource_filename('tethys', 'reference/grid_info/bfracFAO2005.csv')
+
+        self.goat_fract = os.path.join(self.InputFolder, self.map_params.get('Goat_Fraction', 'None'))
+        if os.path.basename(self.goat_fract) == 'None':
+            if self.GCAMUSA == 1:
+                self.goat_fract = pkg_resources.resource_filename('tethys', 'reference/grid_info/gfracFAO2005_gcamUSA.csv')
+            else:
+                self.goat_fract = pkg_resources.resource_filename('tethys', 'reference/grid_info/gfracFAO2005.csv')
+
+        self.irrigated_fract = os.path.join(self.InputFolder, self.map_params.get('Irrigated_Fract', 'None'))
+        if os.path.basename(self.irrigated_fract) == 'None':
+            if self.GCAMUSA == 1:
+                self.irrigated_fract = pkg_resources.resource_filename('tethys', 'reference/grid_info/irrigation-frac_gcamUSA.csv')
+            else:
+                self.irrigated_fract = pkg_resources.resource_filename('tethys', 'reference/grid_info/irrigation-frac.csv')
 
           # Additional details for GCAM USA (Only if present)
-        try:
-            self.basin_state_area = os.path.join(self.InputFolder, self.map_params.get('BasinStateArea'))
-        except:
-            self.basin_state_area = "placeholder string which will not be read"
+        self.basin_state_area = os.path.join(self.InputFolder, self.map_params.get('BasinStateArea', 'None'))
+        if os.path.basename(self.basin_state_area) == 'None' and self.GCAMUSA == 1:
+            self.basin_state_area = pkg_resources.resource_filename('tethys', 'reference/grid_info/basin_state_area_ratio_gcamUSA.csv')
 
         if self.PerformTemporal:
             self.temporal_params = self.config.get('TemporalDownscaling')
