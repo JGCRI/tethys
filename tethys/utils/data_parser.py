f368d43c75f01eb93cd36b54377df0bfd103c1c3
Isaac Thompson <93336839+ifthompson@users.noreply.github.com>
2022-06-01 17:10:33 -0400

    switched csv reader from numpy.genfromtxt to pandas.read_csv for more speed

--- tethys/utils/data_parser.py
+++ tethys/utils/data_parser.py
@@ -7,8 +7,16 @@ License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
 Copyright (c) 2017, Battelle Memorial Institute
 
 """
-import numpy as np
+import pandas as pd
 
 
 def get_array_csv(filename, skip_header):
-    return np.genfromtxt(filename, delimiter=',', skip_header=skip_header, filling_values='0')
+    # a little roundabout but meant to replicate behavior of previous version that used np genfromtxt
+    if skip_header == 0:
+        header_opt = None
+    else:
+        header_opt = 0
+    data = pd.read_csv(filename, header=header_opt).to_numpy()
+    if data.shape[1] == 1:
+        data = data[:, 0]
+    return data
