a60d106c5636bd90544afa001baf335a863fecfd
crvernon <chrisrvernon@gmail.com>
2024-04-12 09:21:00 -0400

    input viewer class

--- tethys/__init__.py
+++ tethys/__init__.py
@@ -1,4 +1,6 @@
 from .model import *
 from .utils.install_supplement import get_example_data, default_download_dir
+from .utils.spatial import InputViewer
 
-__version__ = "2.0.1"
+
+__version__ = "2.1"
